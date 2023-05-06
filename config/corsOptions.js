1d008e543b89225351ea88e4ab2d2aa29b854178
blueprint-12 <blueprint12@daum.net>
2023-05-06 23:48:52 +0900

    style: 디버깅용, cors 함수 주석 추가

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,6 +1,9 @@
 const allowedOrigins = require("./allowedOrigins");
 
 // array.indexOf = 배열에서 지정된 요소를 찾을 수 있는 첫번째 요소를 반환하고 존재하지 않으면 -1 반환
+
+// TODO: 서드 파티 라이브러리 사용
+// 아래의 코드는 라이브러리에서 제시하는 문법
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
