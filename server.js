c2ade89697d60f45b5bbf4401ae788e774a3d94f
blueprint-12 <blueprint12@daum.net>
2023-05-07 19:30:44 +0900

    feat: 몽고DB 연결 세팅 완료

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
 
-console.log(process.env.NODE_ENV);
+connectDB();
 
 const PORT = process.env.PORT || 3500;
 
@@ -40,4 +42,15 @@ app.all("*", (req, res) => {
 // logger와 다르게 app을 시작하라고 말하기 전에 사용하기 때문에 listen 위에 위치
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`server is running on ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`server is running on ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(
+    "error on mongoDB connection",
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`),
+    "mongoErrLog.log"
+  );
+});
