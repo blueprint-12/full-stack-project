280d421ae8aa55fb53aa0076cad5992ace28e278
blueprint-12 <blueprint12@daum.net>
2023-05-10 23:59:22 +0900

    feat: 라우팅처리 및 공통 컴포넌트 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,35 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
-import Login from "./components/Login";
+import Welcome from "../src/features/auth/Welcome";
+import DashLayout from "./components/DashLayout";
+import Login from "./features/auth/Login";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Public />} />
-        <Route path="login" element={<Login />} />
-      </Route>
-    </Routes>
-  );
+   return (
+      <Routes>
+         <Route path="/" element={<Layout />}>
+            <Route index element={<Public />} />
+            <Route path="login" element={<Login />} />
+            {/* 퍼블릭 라우트 끝 */}
+
+            <Route path="dash" element={<DashLayout />}>
+               <Route index element={<Welcome />} />
+
+               <Route path="notes">
+                  <Route index element={<NotesList />}></Route>
+               </Route>
+
+               <Route path="users">
+                  <Route index element={<UsersList />}></Route>
+               </Route>
+            </Route>
+            {/* Dash 레이아웃 끝*/}
+         </Route>
+      </Routes>
+   );
 }
 
 export default App;
---
+++ frontend/src/components/Layout.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+// ES7 snippet: rafce
+const Layout = () => {
+  return <Outlet />;
+};
+
+export default Layout;
