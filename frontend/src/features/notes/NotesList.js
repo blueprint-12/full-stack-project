d9d65524f27f531890d1d703fce3365e44afbdc2
blueprint-12 <blueprint12@daum.net>
2023-05-11 19:04:17 +0900

    feat: 노트, 유저 컴포넌트 생성 및 RTK query 훅을 통한 display 분기

--- frontend/src/features/notes/NotesList.js
+++ frontend/src/features/notes/NotesList.js
@@ -1,7 +1,60 @@
-import React from "react";
+import { useGetNotesQuery } from "./notesApiSlice";
+import Note from "./Note";
 
 const NotesList = () => {
-   return <h1>NotesList</h1>;
+   const {
+      data: notes,
+      isLoading,
+      isError,
+      isSuccess,
+      error,
+   } = useGetNotesQuery();
+
+   let content;
+
+   if (isLoading) content = <p> Loading contents ... </p>;
+
+   if (isError) {
+      content = <p className="errmsg">{error?.data?.message}</p>;
+   }
+   if (isSuccess) {
+      // 서버에서 받아온 응답값(data)의 ids 구조분해로 가져옴
+      const { ids } = notes;
+
+      const tableContent = ids?.length
+         ? ids.map((noteId) => <Note key={noteId} noteId={noteId} />)
+         : null;
+
+      content = (
+         // 미디어 쿼리 적용됨
+         <table className="table table--notes">
+            <thead className="table__thead">
+               <tr>
+                  <th scope="col" className="table__th note__status">
+                     Username
+                  </th>
+                  <th scope="col" className="table__th note__created">
+                     Created
+                  </th>
+                  <th scope="col" className="table__th note__updated">
+                     Updated
+                  </th>
+                  <th scope="col" className="table__th note__title">
+                     Title
+                  </th>
+                  <th scope="col" className="table__th note__username">
+                     Owner
+                  </th>
+                  <th scope="col" className="table__th note__edit">
+                     Edit
+                  </th>
+               </tr>
+            </thead>
+            <tbody>{tableContent}</tbody>
+         </table>
+      );
+   }
+   return content;
 };
 
 export default NotesList;
