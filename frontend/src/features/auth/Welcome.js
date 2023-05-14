625b10e0b2adb419ee1a3db939edf243d1a19cf6
blueprint-12 <blueprint12@daum.net>
2023-05-14 22:00:39 +0900

    feat: 새 노트 생성, 노트 수정 기능

--- frontend/src/features/auth/Welcome.js
+++ frontend/src/features/auth/Welcome.js
@@ -2,29 +2,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Welcome = () => {
-   const date = new Date();
-   const today = new Intl.DateTimeFormat("ko-KR", {
-      dateStyle: "full",
-      timeStyle: "long",
-   }).format(date);
+  const date = new Date();
+  const today = new Intl.DateTimeFormat("ko-KR", {
+    dateStyle: "full",
+    timeStyle: "long",
+  }).format(date);
 
-   const content = (
-      <section className="welcome">
-         <p>{today}</p>
+  const content = (
+    <section className="welcome">
+      <p>{today}</p>
 
-         <h1>Welcome!</h1>
+      <h1>Welcome!</h1>
 
-         <p>
-            <Link to="/dash/notes">View techNotes</Link>
-         </p>
+      <p>
+        <Link to="/dash/notes">테크 노트보기</Link>
+      </p>
+      <p>
+        <Link to="/dash/notes/new">새로운 노트 작성하기</Link>
+      </p>
 
-         <p>
-            <Link to="/dash/users">View User Settings</Link>
-         </p>
-      </section>
-   );
+      <p>
+        <Link to="/dash/users">유저 세팅 보기</Link>
+      </p>
+      <p>
+        <Link to="/dash/users/new">새로운 유저 추가하기</Link>
+      </p>
+    </section>
+  );
 
-   return content;
+  return content;
 };
 
 export default Welcome;
